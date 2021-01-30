5b1aeb817e250522162bacc13b3614f252266ff5
xuankuzcr <18846640555@163.com>
2021-01-31 02:53:54 +0800

    real-time

--- src/img_repub.cpp
+++ src/img_repub.cpp
@@ -8,8 +8,8 @@ using namespace std;
 
 double scale;
 string camera_topic_in,camera_topic_out;
-sensor_msgs::ImagePtr msg_output;
 
+image_transport::Publisher pub;
 void imageCallback(const sensor_msgs::ImageConstPtr& msg)
 {
   try
@@ -17,12 +17,14 @@ void imageCallback(const sensor_msgs::ImageConstPtr& msg)
     //ROS_INFO("The image is subscribing.");
     cv::Mat src = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8)->image;
     cv::Mat dst;
+    sensor_msgs::ImagePtr msg_output;
     cv::resize(src,dst,Size(src.cols*scale,src.rows*scale),0,0,INTER_LINEAR);
-    cv::imshow("Display",src);
-    cv::imshow("Result",dst);
-    if(cv::waitKey(10)==27)
-       exit(0);
+    // cv::imshow("Display",src);
+    // cv::imshow("Result",dst);
+    // if(cv::waitKey(10)==27)
+    //    exit(0);
     msg_output = cv_bridge::CvImage(msg->header, "bgr8", dst).toImageMsg();
+    pub.publish(msg_output);
   }
   catch (cv_bridge::Exception& e)
   {
@@ -52,14 +54,14 @@ int main(int argc, char **argv)
   ros::Time::init();
   image_transport::ImageTransport it(n);
   image_transport::Subscriber sub = it.subscribe( camera_topic_in , 1, imageCallback);   
-  image_transport::Publisher pub = it.advertise(camera_topic_out, 1);
+  pub = it.advertise(camera_topic_out, 1);
   ros::Rate loop_rate(5);
 
   while(1)
   {   
     //ros::Time time = ros::Time::now();
-    pub.publish(msg_output); 
-    loop_rate.sleep();
+     
+    //loop_rate.sleep();
     ros::spinOnce();
   }
 
